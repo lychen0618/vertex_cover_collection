6cbd4d4f04c95c92ddd311cbf1d940d4c63c5773
lishuai.0613 <lishuai.0613@bytedance.com>
2022-05-15 21:41:52 +0800

    Modify concurrent_queue.

--- common/concurrent_drop_head_queue.h
+++ common/concurrent_drop_head_queue.h
@@ -3,6 +3,7 @@
 #include <glog/logging.h>
 
 #include <condition_variable>
+#include <memory>
 #include <mutex>
 #include <queue>
 
@@ -10,11 +11,14 @@ namespace vcc {
 template <typename RequestType>
 class ConcurrentDropHeadQueue {
 public:
-    ConcurrentDropHeadQueue(size_t capacity) : capacity_(capacity) {}
+    ConcurrentDropHeadQueue(size_t capacity)
+        : capacity_(capacity),
+          mutex_(new std::mutex()),
+          cond_(new std::condition_variable()) {}
     ~ConcurrentDropHeadQueue() = default;
 
     void Push(RequestType req) {
-        std::unique_lock<std::mutex> lock(mutex_);
+        std::unique_lock<std::mutex> lock(*mutex_);
         size_t queue_size = queue_.size();
         if (queue_size >= capacity_) {
             // Queue full. Drop head, i.e., the oldest push request.
@@ -23,13 +27,13 @@ public:
             LOG(WARNING) << "Queue full. Drop head.";
         }
         queue_.push(std::move(req));
-        cond_.notify_one();
+        cond_->notify_one();
     }
 
     RequestType Pop() {
-        std::unique_lock<std::mutex> lock(mutex_);
+        std::unique_lock<std::mutex> lock(*mutex_);
         while (queue_.empty()) {
-            cond_.wait(lock);
+            cond_->wait(lock);
         }
         auto req = std::move(queue_.front());
         queue_.pop();
@@ -38,8 +42,8 @@ public:
 
 private:
     std::queue<RequestType> queue_;
-    std::mutex mutex_;
-    std::condition_variable cond_;
     const size_t capacity_;
+    std::shared_ptr<std::mutex> mutex_;
+    std::shared_ptr<std::condition_variable> cond_;
 };
 }  // namespace vcc
