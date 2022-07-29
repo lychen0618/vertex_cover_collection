3ddcf79a3ae931dd88d18a2d556452972426ca69
lishuai.0613 <lishuai.0613@bytedance.com>
2022-07-29 20:25:30 +0800

    Fix compile error.

--- common/approximate_functions.h
+++ common/approximate_functions.h
@@ -3,7 +3,7 @@
 #include "hypergraph.h"
 
 namespace vcc {
-double f1(std::shared_ptr<HyperGraph> hyper_graph, int uncov) {
+inline double f1(std::shared_ptr<HyperGraph> hyper_graph, int uncov) {
     return 1 - uncov * 1.0 / hyper_graph->EdgeNum();
 }
 }  // namespace vcc
